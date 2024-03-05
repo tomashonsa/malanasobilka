979c357e6069741868f3af8c60789f3f4db8320f
tomashonsa <tomasekhonsa@email.cz>
2024-03-05 13:11:40 +0100

    j

--- malanasobilkaa.py
+++ malanasobilkaa.py
@@ -1,22 +1,22 @@
 import random
-for vysledek in range (10):
-    print (vysledek)
+
 
 def nasobeni (x, y):
     vysledek = x * y
     return vysledek
 
-def kontrola(vysledek, volba):
-    if vysledek == volba:
+def kontrola(vysledek, vysledek_zak):
+    if vysledek == vysledek_zak:
         print("Jsi šikulka máš to správně")
     else:
         print ("Jejda mas to spatne")
-
 
 x = random.randint(1,10)
 y = random.randint(1,10)
 vysledek = nasobeni(x,y)
 
+vysledek_zak = input(f"{x} * {y} = ")
+vysledek_zak = int(vysledek_zak)
+kontrola(vysledek, vysledek_zak)
 
-volba = input(f"{x} * {y} = ")
 
