43f9f31e93f56732704382a092972046a587ddbd
tomashonsa <152276276+tomashonsa@users.noreply.github.com>
2024-03-15 12:23:41 +0100

    Update malanasobilkaa.py

--- malanasobilkaa.py
+++ malanasobilkaa.py
@@ -11,12 +11,17 @@ def kontrola(vysledek, vysledek_zak):
     else:
         print ("Jejda mas to spatne")
 
+
+
 x = random.randint(1,10)
 y = random.randint(1,10)
+
+
 vysledek = nasobeni(x,y)
 
 vysledek_zak = input(f"{x} * {y} = ")
 vysledek_zak = int(vysledek_zak)
 kontrola(vysledek, vysledek_zak)
 
-
+for i in range(10):
+    
